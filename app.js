f5118f88520661fab0e0733c785adbdfeb463b9a
zaid <zaidalshreef@gmail.com>
2021-08-10 00:19:04 +0300

    new

--- app.js
+++ app.js
@@ -12,12 +12,22 @@ function dinosaurConstructor(species, weight, height, diet, where, when, fact) {
 }
 
 // Create Dino Objects
-const Dinosaurs = [];
-const dions = async () => {
-    const req = await fetch(".dino.json");
+let dinosaurs = [];
+(async function getdino() {
+    const req = await fetch("./dino.json");
     const result = await req.json();
-    return result.Dinos;
-};
+    let dinos = result.Dinos;
+    dinos.forEach((dino)=>{
+        dinosaurs.push(new dinosaurConstructor(
+        dino.species,
+        dino.weight,
+        dino.height,
+        dino.diet,
+        dino.where,
+        dino.when,
+        dino.fact,
+        ))})})();
+
 
 // Create Human Object
 function Human(Name, Height, Weight, Diet) {
@@ -26,20 +36,7 @@ function Human(Name, Height, Weight, Diet) {
     this.weight = Weight;
     this.diet = Diet;
 }
-let human = new Human();
-human = (
     // Use IIFE to get human data from form
-    function gethuman() {
-        human.name = document.getElementById("name").value;
-        const feetHeight = document.getElementById("feet").value;
-        const inchesHeight = document.getElementById("inches").value;
-        human.height = feetHeight * 12 + inchesHeight;
-        human.weight  = document.getElementById("weight").value;
-        human.diet = document.getElementById("diet").value;
-        return human;
-    }
-)();
-console.log(human);
 
 const dinoMethods = {
     compareWeight: function (humanWeight) {
@@ -84,8 +81,52 @@ const dinoMethods = {
     // NOTE: Weight in JSON file is in lbs, height in inches.
 
 
+function genratetile(humaninfo){
+
+    const fragment = new DocumentFragment();
+    let counter = 0
+for(let i = 0 ; i < 9 ;i++) {
+    if (i == 4) {
+        // insert human
+        const humandiv = document.createElement('div');
+        humandiv.className = 'grid-item';
+        humandiv.innerHTML = `<h3>${humaninfo.name}</h3><img src="images/human.png" alt="human image">`;
+        fragment.appendChild(humandiv);
+    }else {
+        const dinoDiv = document.createElement("div");
+        dinoDiv.className = "grid-item";
+        dinoDiv.innerHTML = `<h3>${dinosaurs[counter].species}</h3><img src="images/${dinosaurs[counter].species.toLowerCase()}.png" alt="${dinosaurs[counter].species} image"><p>${dinosaurs[counter].fact}</p>`;
+        fragment.appendChild(dinoDiv);
+        counter++;
+    }
+
+}
+    document.getElementById("grid").appendChild(fragment);
+    document.querySelector("form").style.display = "none";
+    document.querySelector(".retry").classList.remove("hide");
+    document.querySelector(".new-fact").classList.remove("hide");
+    document.querySelector(".new-fact").style.display = "inline block";
+    document.getElementById("grid").style.display = "flex";
+}
     // Generate Tiles for each Dino in Array
-  
+
+document.getElementById("btn").addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(dinosaurs)
+    const humaninfo = (function gethuman() {
+            let human = new Human();
+            human.name = document.getElementById("name").value;
+            const feetHeight = document.getElementById("feet").value;
+            const inchesHeight = document.getElementById("inches").value;
+            human.height = feetHeight * 12 + inchesHeight;
+            human.weight  = document.getElementById("weight").value;
+            human.diet = document.getElementById("diet").value;
+            return human;
+        }
+    )();
+    genratetile(humaninfo);
+});
+
         // Add tiles to DOM
 
     // Remove form from screen
